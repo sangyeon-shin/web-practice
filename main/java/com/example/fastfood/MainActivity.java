9619e363ca9b9f750f549c20986abc66d2353a55
sagnyeon-shin <68421463+sagnyeon-shin@users.noreply.github.com>
2021-03-09 16:28:41 +0900

    FastFood 어플

---
+++ main/java/com/example/fastfood/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.fastfood;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                //select logout item
+                break;
+            case R.id.account:
+                //select account item
+                break;
+            case R.id.exit:
+                //select back button
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
